7c478d0c128f8441d6ba1655d7b7acd3871035cc
usman <akanbiusman5@gmail.com>
2026-02-15 00:36:20 +0100

    guess game

--- python/main.py
+++ python/main.py
@@ -1,3 +1,30 @@
-name = input("what is your name? ")
-color = input("what is your favorite color? ")
-print(name+" likes "+color)
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    
+    if guess == secret_number:
+        print("You won!")
+        break
+
+else:
+    print("Sorry, You failed!")
+
+
+
+# print("Weight Converter")
+# print("-"*len("Weight Converter"))
+
+# user_weight = int(input("Weight: "))
+
+# user_unit = str(input("(L)bs or (K)g: "))
+
+# if user_unit.lower() == "l":
+#     print(f"{user_weight * 0.45}pounds")
+# elif user_unit.lower() == "k":
+#     print(f"{user_weight/0.45}kilos")
+# else:
+#     print("you didn't pick the right choice of unit!")
