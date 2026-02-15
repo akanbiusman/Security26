c8b925463300f4e071e999feaf8f7d78406d1c22
usman <akanbiusman5@gmail.com>
2026-02-15 18:32:31 +0100

    car game

--- python/main.py
+++ python/main.py
@@ -1,17 +1,39 @@
-secret_number = 9
-guess_count = 0
-guess_limit = 3
+help_inp = input("Press 'help' to get a list of car operations: ")
+print("Below are some commands")
+print("-"*len("Below are some commands"))
+print("start - to start the car")
+print("stop - to stop the car")
+print("quit - to exit")
 
-while guess_count < guess_limit:
-    guess = int(input("Guess: "))
-    guess_count += 1
+while help_inp.lower() == 'help':
+    cmd_now = input(">").lower().strip()
     
-    if guess == secret_number:
-        print("You won!")
+    if cmd_now == "start":
+        print("Car started...Ready to go!")
+    elif cmd_now == "stop":
+        print("Car stopped")
+    elif cmd_now == "quit":
         break
+    else:
+        print("I don't understand that...")
 
-else:
-    print("Sorry, You failed!")
+
+
+
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+
+# while guess_count < guess_limit:
+#     guess = int(input("Guess: "))
+#     guess_count += 1
+    
+#     if guess == secret_number:
+#         print("You won!")
+#         break
+
+# else:
+#     print("Sorry, You failed!")
 
 
 
