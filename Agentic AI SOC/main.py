968863bb17b08c7201c1f3e2fa41a3dd162729f5
usman <akanbiusman5@gmail.com>
2026-02-20 18:06:36 +0100

    people json

---
+++ Agentic AI SOC/main.py
@@ -0,0 +1,53 @@
+people = [
+    {
+        "name": "Josh",
+        "age": 35,
+        "profession": "Cyber Professional",
+        "locations": ["United States", "Thailand", "Japan"],
+        "likes": [
+            "Japanese sweet potato",
+            "cakes",
+            "pizza",
+            "sushi"
+        ]
+    },
+    {
+        "name": "Maya",
+        "age": 32,
+        "profession": "Cyber Professional",
+        "locations": ["United States", "Thailand", "Japan"],
+        "likes": [
+            "Japanese sweet potato",
+            "cakes",
+            "pizza",
+            "sushi"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "age": 30,
+        "profession": "Cyber Professional",
+        "locations": ["United States", "Thailand", "Japan"],
+        "likes": [
+            "Japanese sweet potato",
+            "cakes",
+            "pizza",
+            "sushi"
+        ]
+    },
+    {
+        "name": "Aiko",
+        "age": 40,
+        "profession": "Cyber Professional",
+        "locations": ["United States", "Thailand", "Japan"],
+        "likes": [
+            "Japanese sweet potato",
+            "cakes",
+            "pizza",
+            "sushi"
+        ]
+    }
+]
+
+
+print(people[3]["locations"][1])
