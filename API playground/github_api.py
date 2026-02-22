92971479b316e98dfdbe698361b360bcf48c0975
usman <akanbiusman5@gmail.com>
2026-02-22 16:34:14 +0100

    github-api

--- API playground/github_api.py
+++ API playground/github_api.py
@@ -1,13 +1,29 @@
 import requests
 
-username = input("Enter a Github username: ")
 
-response = requests.get(f"https://api.github.com/users/{username}/repos")
+def get_repos():
+    github_username = input("Enter a Github username: ")
 
-data = response.json()
-print(data)
+    response = requests.get(
+        f"https://api.github.com/users/{github_username}/repos")
 
-# for key in data:
-#     print(data['name'])
-#     print(data['description'])
-#     print(data['html_url'])
+    data = response.json()
+
+    if 'message' in data and data['message'] == 'Not Found':
+        print("Username not found!")
+
+        return None
+
+    return data
+
+
+data = get_repos()
+
+if data is None:
+    exit()
+
+for repo in data:
+    print(repo['name'])
+    print(repo['description'])
+    print(repo['html_url'])
+    print("\n")
